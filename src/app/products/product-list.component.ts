77ad2c24d196681dcabed8b56aa348029c453023
jontecodes <jrufus@dciinc.org>
2020-02-07 15:39:32 -0600

    Initial Commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { IProduct } from './product';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+    pageTitle = 'Product List';
+    imageWidth = 50;
+    imageMargin = 2;
+    showImage = false;
+    listFilter = 'cart';
+
+    products: IProduct[] = [
+        {
+            productId: 2,
+            productName: 'Garden Cart',
+            productCode: 'GDN-0023',
+            releaseDate: 'March 18, 2019',
+            description: '15 gallon capacity rollonh garden',
+            price: 32.99,
+            starRating: 4.2,
+            imageUrl: 'assets/images/garden_cart.png'
+        },
+        {
+            productId: 5,
+            productName: 'Hammer',
+            productCode: 'TBX-0048',
+            releaseDate: 'March 21, 2019',
+            description: 'Curved claw steel hammer',
+            price: 8.9,
+            starRating: 4.8,
+            imageUrl: 'assets/images/hammer.png'
+        },
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+}
