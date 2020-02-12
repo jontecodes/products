cec381c630f44ca12787ff258216fea811ab221a
jontecodes <jrufus@dciinc.org>
2020-02-12 14:12:44 -0600

    Bug fixes

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from '../product';
+import { from } from 'rxjs';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,9 +12,17 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    console.log('Hello');
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
